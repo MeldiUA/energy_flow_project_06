93f476f6d3402964899d28890e02a17bbcdc9bf1
MeldiUA <dimamelnichuk11.04.1999@gmail.com>
2024-01-28 16:33:09 +0200

    add normalaizer and local storage logical

---
+++ src/js/localalStorageLogical.js
@@ -0,0 +1,20 @@
+export const LS_FAV = 'favourite';
+
+export const setFav = arr => {
+  localStorage.setItem(LS_FAV, JSON.stringify(arr));
+};
+
+export const getFav = key => {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    console.log(err.message);
+    //   add notification here in future instead of log
+  }
+};
+
+export const removeFromFav = id => {
+  const filteredArr = getFav(LS_FAV).filter(obj => obj.id !== id);
+  localStorage.removeItem(LS_FAV);
+  localStorage.setItem(LS_FAV, JSON.stringify(filteredArr));
+};
